0b85d84b86a3511fddcc0188d124a94250e1ec04
Luis Garcia Gomez <luigarg5@ei.upv.es>
2015-02-19 21:41:47 +0100

    Implementación PriorityQColaPrioridad<E> Versión 1.1

--- eda/librerias/estructurasDeDatos/jerarquicos/PriorityQColaPrioridad.java
+++ eda/librerias/estructurasDeDatos/jerarquicos/PriorityQColaPrioridad.java
@@ -19,25 +19,53 @@ public class PriorityQColaPrioridad<E extends Comparable<E> >
      * according to their natural ordering.*/
      
     /** crea una Cola de Prioridad (CP) vacia */
-    public PriorityQColaPrioridad() { super(0); }
+    public PriorityQColaPrioridad() { 
+        
+        //Llamada explicita al constructor del padre.
+        super(0);   //Puede que de error.
+        
+        //Versión alternativa con el constructor por defecto del padre que crea Cola con 11 elementos.
+        //Luego se borran con el clear() del padre.
+        //Llamada implicita al constructor del padre.
+        
+        //Recordemos que super() no se usa para invocar a los metodos heredados!!!
+       /* super();
+        this.clear();*/
+    }   
     
     /** atendiendo a su prioridad, inserta el Elemento e en una Cola de Prioridad (CP)
      *  @param e Elemento a agnadir a una Cola de Prioridad
      */
-    public void insertar(E e) { /*COMPLETAR*/ }
+    public void insertar(E e) { 
+        /*add(E e) Inserts the specified element into this priority queue.*/
+         
+        this.add(e); }
     
     /** SII !esVacia(): obtiene el Elemento con maxima prioridad de una CP 
      * @return E Elemento con maxima prioridad de una CP
      */
-    public E recuperarMin() { /*COMPLETAR*/ }
+    public E recuperarMin() { 
+        /* peek() Retrieves, but does not remove, the head of this queue,
+         * or returns null if this queue is empty.*/
+         
+         return this.peek();
+    }
     
      /** SII !esVacia(): obtiene y elimina el Elemento con maxima prioridad de una CP 
      *  @return E Elemento con maxima prioridad de una CP
      */
-    public E eliminarMin() { /*COMPLETAR*/  }
+    public E eliminarMin() {
+        /* poll() Retrieves and removes the head of this queue,
+         * or returns null if this queue is empty.*/
+         
+         return this.poll();
+          }
     
     /** comprueba si una Cola de Prioridad esta vacia
       * @return true si una CP esta vacia y false en caso contrario
       */
-    public boolean esVacia() { /*COMPLETAR*/ }
+    public boolean esVacia() {
+        /*size() Returns the number of elements in this collection.*/
+         return this.size() == 0;
+    }
 }
