7a168f7b9abb1282824eb0b7ab58263e6635e9e4
Luis Garcia Gomez <luigarg5@ei.upv.es>
2015-03-02 19:58:44 +0100

    Versión todo correcto práctica 1 sesión 2, Versión: 1.2

--- eda/librerias/estructurasDeDatos/lineales/LEGListaConPI.java
+++ eda/librerias/estructurasDeDatos/lineales/LEGListaConPI.java
@@ -20,69 +20,92 @@ public class LEGListaConPI<E> implements ListaConPI<E> {
     // En principio, una LEGListaConPI NO tendria una talla 
     // ... PERO se incluye pensando en la futura reutilizacion del Modelo
     protected int talla;
-	
-	/** construye una Lista Con PI vacia **/
-	public LEGListaConPI() {
-		pri = ult = ant = new NodoLEG<E>(null);
-		talla = 0;
-	}
-	
-	/** inserta e en una Lista antes del Elemento que ocupa su PI, que permanece inalterado
-	  * @param e Elememto a insertar
-	  */
-	public void insertar(E e) { 
-		NodoLEG<E> nuevo = new NodoLEG<E>(e); talla++;
-		// Se inserta nuevo DETRAS de ant, i.e. ANTES del Dato que ocupa el PI
-		/*COMPLETAR*/
-		// OJO: al insertar en fin de Lista ... actualizar ult!
-		/*COMPLETAR*/
-		// OJO: tras la insercion el PI de la Lista permanece inalterado
-		/*COMPLETAR*/
-		
-	} 
-	
-	/** SII !esFin(): elimina de una Lista el Elemento que ocupa su PI, que permanece inalterado **/  
-	public void eliminar() {
-	     	talla--;
-	        // OJO: al eliminar el ultimo Elemento ... actualizar ult!; el PI esta en fin()
-            /*COMPLETAR*/
-	}
-	
-	/** situa el PI de una Lista en su inicio, sobre su primer Elemento si no esta vacia **/
-	public void inicio() { /*COMPLETAR*/ }
-	
-	/** SII !esFin(): situa sobre el siguiente Elemento el PI de una Lista  **/
-	public void siguiente() { /*COMPLETAR*/ }
-	
-	/** situa el PI de una Lista en su fin, detras de su ultimo Elemento si no esta vacia **/
-	public void fin() { /*COMPLETAR*/ }
-	
-	/** SII !esFin(): obtiene el Elemento que ocupa el PI de una Lista
-	 *  @return E, el Elemento que ocupa el PI de una Lista
-	 */
-	public E recuperar() { /*COMPLETAR*/ }
-	
-	/** comprueba si el PI de una Lista esta en su fin
-	 *  @return true si el PI de una Lista esta en su fin y false en caso contrario
-	 */
-	public boolean esFin() { /*COMPLETAR*/ }
-	
-	/** comprueba si una Lista Con PI esta vacia
-	 *  @return true si una Lista Con PI esta vacia y false en caso contrario
-	 */
-	public boolean esVacia() { /*COMPLETAR*/ }
-	
-	/** devuelve la talla de una Lista Con PI, i.e. su numero de Elementos 
-	 *  @return int, el numero de Elementos de una Lista con PI
-	 */
+    
+    /** construye una Lista Con PI vacia **/
+    public LEGListaConPI() {
+        pri = ult = ant = new NodoLEG<E>(null);
+        talla = 0;
+    }
+    
+    /** inserta e en una Lista antes del Elemento que ocupa su PI, que permanece inalterado
+      * @param e Elememto a insertar
+      */
+    public void insertar(E e) { 
+        NodoLEG<E> nuevo = new NodoLEG<E>(e); 
+        talla++;
+        // Se inserta nuevo DETRAS de ant, i.e. ANTES del Dato que ocupa el PI
+        nuevo.siguiente = ant.siguiente;
+        ant.siguiente = nuevo;
+        // OJO: al insertar en fin de Lista ... actualizar ult!
+        if (ant == ult){
+            ult = nuevo;
+          }
+        // OJO: tras la insercion el PI de la Lista permanece inalterado
+        ant = nuevo;
+        
+    } 
+    
+    /** SII !esFin(): elimina de una Lista el Elemento que ocupa su PI, que permanece inalterado **/  
+    public void eliminar() {
+            talla--;
+            // OJO: al eliminar el ultimo Elemento .    a.. actualizar ult!; el PI esta en fin()
+            if (ant.siguiente == ult) {
+                ult = ant;
+            }
+                
+            ant.siguiente = ant.siguiente.siguiente;
+    }
+    
+    /** situa el PI de una Lista en su inicio, sobre su primer Elemento si no esta vacia **/
+    public void inicio() { 
+        ant = pri; 
+    }
+    
+    /** SII !esFin(): situa sobre el siguiente Elemento el PI de una Lista  **/
+    public void siguiente() { 
+        ant = ant.siguiente;
+    }
+    
+    /** situa el PI de una Lista en su fin, detras de su ultimo Elemento si no esta vacia **/
+    public void fin() { 
+        ant = ult;
+    }
+    
+    /** SII !esFin(): obtiene el Elemento que ocupa el PI de una Lista
+     *  @return E, el Elemento que ocupa el PI de una Lista
+     */
+    public E recuperar() { 
+        E dato = ant.siguiente.dato;
+        return dato;
+}
+    
+    /** comprueba si el PI de una Lista esta en su fin
+     *  @return true si el PI de una Lista esta en su fin y false en caso contrario
+     */
+    public boolean esFin() { 
+        return ant == ult;
+    }
+    
+    /** comprueba si una Lista Con PI esta vacia
+     *  @return true si una Lista Con PI esta vacia y false en caso contrario
+     */
+    public boolean esVacia() { 
+        return pri == ult;
+        //Otra forma que funciona:
+        //return pri.siguiente == null; 
+    }
+    
+    /** devuelve la talla de una Lista Con PI, i.e. su numero de Elementos 
+     *  @return int, el numero de Elementos de una Lista con PI
+     */
      public int talla() { return talla; }
-	
+    
     /** devuelve el String con los Elementos de una Lista con PI en orden de insercion 
      *   @return String que contiene los Elementos de una Lista con PI, en el mismo 
      *   formato que usa el estandar de Java para los arrays
      */
-	public String toString() {
-		// NOTA: se usa la clase StringBuilder, en lugar de String, por motivos de eficiencia 
+    public String toString() {
+        // NOTA: se usa la clase StringBuilder, en lugar de String, por motivos de eficiencia 
         StringBuilder s = new StringBuilder();
         s.append("[");
         NodoLEG<E> aux = pri.siguiente; 
